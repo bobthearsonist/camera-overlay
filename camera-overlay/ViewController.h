6d2cd439f277b2857870d8c19c96576196776146
Martin Perry <martinperry@gatech.edu>
2016-12-10 15:55:02 -0500

    Initial Commit

---
+++ camera-overlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  camera-overlay
+//
+//  Created by admin on 12/10/16.
+//  Copyright © 2016 KSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
