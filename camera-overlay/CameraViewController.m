9be8cb1c85626198abcb33fcf16fdd262a05e86d
Martin Perry <martinperry@gatech.edu>
2016-12-10 16:58:35 -0500

    fancy stuff

--- camera-overlay/CameraViewController.m
+++ camera-overlay/CameraViewController.m
@@ -22,6 +22,35 @@
     [self.view.layer addSublayer:self.captureLayer];
 }
 
+- (void)viewDidLayoutSubviews
+{
+    CGRect bounds=self.captureLayer.bounds;
+    self.captureLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    self.captureLayer.bounds=bounds;
+    self.captureLayer.position=CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds));
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator
+{
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    // Code here will execute before the rotation begins.
+    // Equivalent to placing it in the deprecated method -[willRotateToInterfaceOrientation:duration:]
+    
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+        // Place code here to perform animations during the rotation.
+        // You can pass nil or leave this block empty if not necessary.
+        
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+        // Code here will execute after the rotation has finished.
+        // Equivalent to placing it in the deprecated method -[didRotateFromInterfaceOrientation:]
+        [self updateCameraRotation];
+        //self.captureLayer.frame = self.view.bounds;
+    }];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -44,6 +73,45 @@
     self.captureLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
     
     [self.captureSession startRunning];
+}
+
+-(void) updateCameraRotation
+{
+    AVCaptureConnection *previewLayerConnection=self.captureLayer.connection;
+    if ([previewLayerConnection isVideoOrientationSupported])
+    {
+        [previewLayerConnection setVideoOrientation:[self videoOrientationFromCurrentViewOrientation]];
+    }
+}
+
+- (AVCaptureVideoOrientation) videoOrientationFromCurrentViewOrientation {
+    AVCaptureVideoOrientation orientation;
+    
+    switch ([[UIApplication sharedApplication] statusBarOrientation])
+    {
+        case UIInterfaceOrientationPortrait:
+        {
+            orientation = AVCaptureVideoOrientationPortrait;
+        }
+        case UIInterfaceOrientationLandscapeLeft:
+        {
+            orientation = AVCaptureVideoOrientationLandscapeLeft;
+        }
+        case UIInterfaceOrientationLandscapeRight:
+        {
+            orientation = AVCaptureVideoOrientationLandscapeRight;
+        }
+        case UIInterfaceOrientationPortraitUpsideDown:
+        {
+            orientation = AVCaptureVideoOrientationPortraitUpsideDown;
+        }
+        default:
+        {
+            orientation = AVCaptureVideoOrientationPortrait;
+        }
+    }
+    
+    return orientation;
 }
 
 /*
